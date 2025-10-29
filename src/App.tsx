91d0c08313f68750777eb77f232f21f4a25025f7
im-ivan <ivanhfc00@gmail.com>
2025-10-29 16:17:38 -0300

    ft: element filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
 import "./App.css";
+import data from "./assets/data/periodic-table-elements.json";
+
+type ElementFromJson = (typeof data.elements)[number];
+const elements: ElementFromJson[] = data.elements;
+
+interface IElementFieldProps {
+  element: ElementFromJson;
+}
+
+const ElementField = ({ element }: IElementFieldProps) => {
+  return (
+    <tr key={element.number}>
+      <td>{element.name}</td>
+      <td>{element.appearance}</td>
+    </tr>
+  );
+};
 
 function App() {
-  return <></>;
+  const [filter, filterSet] = useState("");
+  return (
+    <>
+      <h1 className="text-center border">Title</h1>
+      <input
+        type="text"
+        value={filter}
+        onChange={(ev) => filterSet(ev.target.value)}
+      />
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Appearence</th>
+          </tr>
+        </thead>
+        <tbody>
+          {elements
+            .filter((element) => element.name.includes(filter))
+            .slice(0, 11)
+            .map((element) => (
+              <ElementField key={element.number} element={element} />
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default App;
